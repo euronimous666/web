28c31878bc1bdfd79223f33cee978138c4c6e8ba
euronimous666 <emerzon.emerzondavid@gmail.com>
2020-06-25 16:40:04 -0400

    sitio web

---
+++ js/main.js
@@ -0,0 +1,9 @@
+document.querySelector('.menu-btn').addEventListener('click', () =>{
+    document.querySelector('.nav-menu').classList.toggle('show');
+
+});
+
+ScrollReveal().reveal('.showcase');
+ScrollReveal().reveal('.news-cards',{ delay: 500});
+ScrollReveal().reveal('.car-banner-one',{ delay: 500});
+ScrollReveal().reveal('.car-banner-two',{ delay: 500});
